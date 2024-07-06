29cff89edbe69f019e24c550c84c04ec69dbcbe8
yurispeondivision <yurispeondivision@proton.me>
2024-07-06 16:17:04 +0100

    Add GI enemy assets

--- Scripts/Entities/Enemy/Enemy.cs
+++ Scripts/Entities/Enemy/Enemy.cs
@@ -4,11 +4,14 @@ namespace RA2Survivors
 {
     public abstract partial class Enemy : Entity
     {
+        protected RA2Sprite3D Sprite; 
+
         public override void _Ready()
         {
             base._Ready();
             GamemodeLevel1.Instance.totalEnemiesCount++;
             GamemodeLevel1.Instance.enemyCount[(int)associatedEntity]++;
+			Sprite = (RA2Sprite3D)FindChild("Sprite3D");
         }
 
         // public override void _PhysicsProcess(double delta)
@@ -33,6 +36,42 @@ namespace RA2Survivors
             ).Normalized();
             Vector3 velocity = direction * (float)stats.movementSpeed;
             state.LinearVelocity = velocity;
+
+			if (velocity.X == 0 && velocity.Z == 0)
+			{
+				Sprite.PlayAnim("face_s");
+			} else if (velocity.X == 0 && velocity.Z < 0)
+			{
+				Sprite.PlayAnim("run_n");
+			}
+			else if (velocity.X < 0 && velocity.Z < 0)
+			{
+				Sprite.PlayAnim("run_nw");
+			}
+			else if (velocity.X < 0 && velocity.Z == 0)
+			{
+				Sprite.PlayAnim("run_w");
+			}
+			else if (velocity.X < 0 && velocity.Z > 0)
+			{
+				Sprite.PlayAnim("run_sw");
+			}
+			else if (velocity.X == 0 && velocity.Z > 0)
+			{
+				Sprite.PlayAnim("run_s");
+			}
+			else if (velocity.X > 0 && velocity.Z > 0)
+			{
+				Sprite.PlayAnim("run_se");
+			}
+			else if (velocity.X > 0 && velocity.Z == 0)
+			{
+				Sprite.PlayAnim("run_e");
+			}
+			else if (velocity.X > 0 && velocity.Z < 0)
+			{
+				Sprite.PlayAnim("run_ne");
+			}
         }
 
         protected override void Dispose(bool disposing)
--- Scripts/Entities/Enemy/GI.cs
+++ Scripts/Entities/Enemy/GI.cs
@@ -17,6 +17,52 @@ namespace RA2Survivors
 				expDropped = 1,
 			};
 			stats.currentHealth = stats.maxHealth;
+			
+			Sprite.AnimDefinitions.Add("face_s",new RA2SpriteAnim() {
+				StartFrame = 4,
+				EndFrame = 4,
+				Loop = true
+			});
+			Sprite.AnimDefinitions.Add("run_n",new RA2SpriteAnim() {
+				StartFrame = 8,
+				EndFrame = 8+5,
+				Loop = true
+			});
+			Sprite.AnimDefinitions.Add("run_nw",new RA2SpriteAnim() {
+				StartFrame = 14,
+				EndFrame = 14+5,
+				Loop = true
+			});
+			Sprite.AnimDefinitions.Add("run_w",new RA2SpriteAnim() {
+				StartFrame = 20,
+				EndFrame = 20+5,
+				Loop = true
+			});
+			Sprite.AnimDefinitions.Add("run_sw",new RA2SpriteAnim() {
+				StartFrame = 26,
+				EndFrame = 26+5,
+				Loop = true
+			});
+			Sprite.AnimDefinitions.Add("run_s",new RA2SpriteAnim() {
+				StartFrame = 32,
+				EndFrame = 32+5,
+				Loop = true
+			});
+			Sprite.AnimDefinitions.Add("run_se",new RA2SpriteAnim() {
+				StartFrame = 38,
+				EndFrame = 38+5,
+				Loop = true
+			});
+			Sprite.AnimDefinitions.Add("run_e",new RA2SpriteAnim() {
+				StartFrame = 44,
+				EndFrame = 44+5,
+				Loop = true
+			});
+			Sprite.AnimDefinitions.Add("run_ne",new RA2SpriteAnim() {
+				StartFrame = 50,
+				EndFrame = 50+5,
+				Loop = true
+			});
 		}
 
 		public override void _PhysicsProcess(double delta)
